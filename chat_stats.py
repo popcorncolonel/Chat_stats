abe866f805b2bdec03ebf089ddf7b2e073f61e36
unknown <popcorncolonel@gmail.com>
2014-07-31 23:59:37 -0400

    Debug mode, better formatted dirs, regular timers (with threading)

--- chat_stats.py
+++ chat_stats.py
@@ -8,6 +8,7 @@
 #NOTE: 'rate' is in messages per minute
 
 verbose = True
+debug = False
 include_emotes = False
 
 import sys
@@ -15,6 +16,7 @@ import os
 import time
 import string
 import datetime
+import threading
 import urllib2
 import json
 from twitch_chat_listen import listen
@@ -24,10 +26,12 @@ except ImportError:
     print "Error loading Twitch password info"
     print "You need to defined pass_info.py with your username and oauth password"
 
-if (len(sys.argv) == 1):
+if len(sys.argv) == 1:
     channel = raw_input("Chat to join: ")
 else:
     channel = sys.argv[1]
+if 'debug' in sys.argv[2:]:
+    debug = True
 
 #thanks to http://twitchemotes.com/ :-)
 def getEmotes():
@@ -46,11 +50,14 @@ emotelist = getEmotes()
 emotelist.remove('GG')
 emotelist.remove('Gg')
 
-directory = "logs/" + channel + '_' + datetime.datetime.now().strftime('%B-%d-%Y_%H-00-00') 
-if not os.path.exists(directory):
+directory = "logs/" + channel + '_' + datetime.datetime.now().strftime('%B-%d-%Y_%I%p') 
+if not os.path.exists(directory) and not debug:
     os.makedirs(directory)
 print 
-print "Writing to " + directory
+if debug:
+    print "Debug mode - not writing to any directories"
+else:
+    print "Writing to " + directory
 print
 
 def open_file(kind, extension='log'):
@@ -59,12 +66,14 @@ def open_file(kind, extension='log'):
     file_path = os.path.relpath(directory + '/' + filename)
     return open(file_path, 'w')
 
-authors = open_file('authors') #to get the most active users
-messages = open_file('messages') #literal log of the messages - for average message length, etc.
-words = open_file('words') #to get word cloud
-emotes = open_file('emotes') #for emote stats
-rate = open_file('rate', 'csv') #how fast chat is going
-files = [authors, messages, words, emotes, rate]
+files = []
+if not debug:
+    authors = open_file('authors') #to get the most active users
+    messages = open_file('messages') #literal log of the messages - for average message length, etc.
+    words = open_file('words') #to get word cloud
+    emotes = open_file('emotes') #for emote stats
+    rate = open_file('rate', 'csv') #how fast chat is going
+    files = [authors, messages, words, emotes, rate]
 
 def isMessage(data):
     return len(data.split('tmi.twitch.tv PRIVMSG #')) > 1
@@ -74,13 +83,12 @@ def formatMessage(message):
     return message.strip().split('\n')[0].split('\r')[0]
 
 num_messages = 0
-minute = 0
 
 def log(author, message):
-    global minute
     global num_messages
-    global last_min
     num_messages += 1
+    if debug:
+        return
     message = formatMessage(message)
 
     #AUTHORS
@@ -96,23 +104,61 @@ def log(author, message):
     for word in message.split(' '):
         if word in emotelist:
             emotes.write(word.split('/')[0].split('7')[0] + '\n')
-    #RATE
-    t = time.time()
-    if t - last_min > 60:
-        r = float(num_messages) / (t-last_min) * 60.0
-        rate.write(str(minute)+','+str(r)+'\n')
-        num_messages = 0
-        minute += 1
-        last_min = t
 
     for f in files:
         f.flush()
 
+
+#http://stackoverflow.com/questions/5179467
+def setInterval(interval, times=-1):
+    # This will be the actual decorator
+    def outer_wrap(function):
+        # This will be the function to be called
+        def wrap(*args, **kwargs):
+            stop = threading.Event()
+
+            # This is another function to be executed
+            # in a different thread to simulate setInterval
+            def inner_wrap():
+                i = 0
+                while i != times and not stop.isSet():
+                    stop.wait(interval)
+                    function(*args, **kwargs)
+                    i += 1
+
+            t = threading.Timer(0, inner_wrap)
+            t.daemon = True
+            t.start()
+            return stop
+        return wrap
+    return outer_wrap
+
+
+#RATE
+@setInterval(60)
+def checkTime():
+    global num_messages
+    rate.write(str(time.strftime("%H:%M"))+','+str(num_messages)+'\n')
+    num_messages = 0
+
 def endProgram():
+    print "closing the program!"
     for f in files:
         f.close()
-#TODO: output stats. (aka the main point of this program.)
+#TODO: output images and stuff. status. (aka the main point of this program.)
+#http://stackoverflow.com/questions/1574088/plotting-time-in-python-with-matplotlib
     sys.exit()
+
+from thread import start_new_thread
+def logEvent(x):
+    try:
+        s = raw_input(x)
+    except EOFError, KeyboardInterrupt, SystemExit:
+        print "==================================ENDING PROGRAM================================"
+        endProgram()
+    start_new_thread(logEvent, ('',))
+
+start_new_thread(logEvent, ('',))
 
 def interpret(data):
     if isMessage(data):
@@ -125,6 +171,9 @@ def interpret(data):
             if verbose:
                 print (author + ' - ' + message).strip()
         except IndexError:
+            print 'MALFORMED DATA - ' + data
+            print 'MALFORMED DATA - ' + data
+            print 'MALFORMED DATA - ' + data
             return
 
 nick = get_username()
@@ -132,8 +181,8 @@ PASS = get_password()
 
 print
 print "===============================STARTING CHAT INPUT=============================="
-last_min = time.time()
+checkTime()
 listen(channel, nick, PASS, interpret)
-print "==================================ENDING PROGRAM================================"
+print "==================================ENDING_PROGRAM================================"
 endProgram()
 
