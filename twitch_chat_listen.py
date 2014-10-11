f4d1be8b6ea2244dc999532f3357e332aade2dd0
popcorncolonel <popcorncolonel@gmail.com>
2014-10-11 00:35:02 -0400

    twitch_chat_listen all primed for general IRC integration

--- twitch_chat_listen.py
+++ twitch_chat_listen.py
@@ -5,14 +5,18 @@
 import socket
 import time
 
-def connect(channel, nick, PASS):
-    join = "#" + channel
+def connect(channel, nick, PASS, server, port):
+    if channel[0] != '#':
+        join = '#' + channel
+    else:
+        join = channel
     while True:
         try:
             sock = socket.socket()
-            sock.connect(("irc.twitch.tv",80))
+            sock.connect((server, port))
             sock.send("USER " + nick + " 0 * :" + nick + "\r\n")
-            sock.send("PASS " + PASS + "\r\n")
+            if PASS != '':
+                sock.send("PASS " + PASS + "\r\n")
             sock.send("NICK " + nick + "\r\n")
             sock.send("MODE " + nick + " +B\r\n")
             sock.send("JOIN " + join + "\r\n")
@@ -26,9 +30,9 @@ def connect(channel, nick, PASS):
 #endFunc is a function that, when called, if it returns true then the loop
 #        will stop. By default (without this function being passed in), 
 #        this program will loop forever when called
-def listen(channel, nick, PASS, interpret, endFunc=None):
+def listen(channel, nick, PASS, interpret, endFunc=None, server='irc.twitch.tv', port=80):
     try:
-        sock = connect(channel, nick, PASS)
+        sock = connect(channel, nick, PASS, server, port)
         sock.settimeout(6) #so we can frequently check for endFunc
         while True:
            data = ''
@@ -56,4 +60,46 @@ def listen(channel, nick, PASS, interpret, endFunc=None):
         print "finishing this"
         raise
         return
+
+if __name__ == '__main__':
+    import sys
+    args = sys.argv[1:]
+# channel, nick, password, [server], [port]
+    print args
+    try:
+        channel = args[0]
+    except IndexError:
+        channel = raw_input('Channel to join: ')
+
+    try:
+        nick = args[1]
+    except IndexError:
+        nick = raw_input('Nick to use: ')
+
+    try:
+        PASS = args[2]
+    except IndexError:
+        PASS = raw_input('Password (might be OAUTH): ')
+
+    try:
+        server = args[3]
+    except IndexError:
+        server = raw_input('Server (if twitch channel, just press enter): ')
+        if server == '': #if it's a twitch channel
+            server = 'irc.twitch.tv'
+            port = 6667
+            if channel != '' and channel[0] != '#':
+                print channel
+                print channel[0]
+                channel = '#' + channel
+
+    if server != 'irc.twitch.tv':
+        try:
+            port = int(args[4])
+        except IndexError:
+            port = int(raw_input('Port: '))
+    print channel, nick, PASS, server, port
+    def interpret(data):
+        print data
+    listen(channel, nick, PASS, interpret, server=server, port=port)
 
